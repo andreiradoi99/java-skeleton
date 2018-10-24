ddce14da464026f8d11f9394e72650f643494620
andreiradoi99 <andrei_radoi14@yahoo.com>
2018-10-24 23:42:19 +0100

    Q1 added

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,27 @@
 package answers;
 
+
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		int maxim=0;
+        int n=portfolios.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                int ans=0,t1=portfolios[i],t2=portfolios[j],p2=1;
+                while((t1!=0)||(t2!=0))
+                {
+                    if((t1%2+t2%2)==1) ans+=p2;
+                    p2*=2;
+                    t1/=2;
+                    t2/=2;
+                }
+                if(maxim<ans) maxim=ans;
+            }
+        }
+		return maxim;
 	}
 
 }
