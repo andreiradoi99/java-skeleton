c1bb2b562e700e2e1cd851bde484656957bddeec
andreiradoi99 <andrei_radoi14@yahoo.com>
2018-10-25 21:59:43 +0100

    q4 updated

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -2,39 +2,45 @@ package answers;
 
 
 
-public class Question4 {
-    public static int getnum(String s)
+
+
+
+
+public class Question4
+{
+    public static int selectionFailedTradedesks(String[][] rows, int numberMachines)
     {
-        int nr=Integer.parseInt(s);
-        return nr;
+        int n=rows.length;
+        int minim=-1;
+        for(int i=0; i<n; i++)
+        {
+            int m=rows[i].length;
+            int t=numberMachines,sum=0;
+            for(int j=0; j<m; j++)
+            {
+                if("X"==rows[i][j])
+                {
+                    t=numberMachines;
+                    sum=0;
+                }
+                else
+                {
+                    sum+=Integer.parseInt(rows[i][j]);
+                    --t;
+                    if(t==0)
+                    {
+                        if(minim==-1) minim=sum;
+                        else if(minim>sum) minim=sum;
+                        sum-=Integer.parseInt(rows[i][j-numberMachines+1]);
+                        t=1;
+                    }
+                }
+            }
+        }
+        if(minim==-1) minim=0;
+        return minim;
     }
-	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		int n=rows.length;
-		int minim=0;
-		for(int i=0;i<n;i++)
-		{
-		    int m=rows[i].length;
-		    int t=numberMachines,sum=0;
-		    for(int j=0;j<m;j++)
-		    {
-		        if("X"==rows[i][j])
-		        {
-		            t=numberMachines;
-		            continue;
-		        }
-		        sum+=getnum(rows[i][j]);
-		        --t;
-		        if(t==0)
-		        {
-		            if(minim==0) minim=sum;
-		            else if(minim>sum) minim=sum;
-		            sum-=getnum(rows[i][j-numberMachines+1]);
-		            t=1;
-		         }
-		    }
-		  }
-		return minim;
-	}
 
 }
+
 
