99adc8e41cffac1886ebcdb3140ddd64ede56cbc
andreiradoi99 <andrei_radoi14@yahoo.com>
2018-10-25 01:32:31 +0100

    added q4

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -1,10 +1,40 @@
 package answers;
 
-public class Question4 {
 
+
+public class Question4 {
+    public static int getnum(String s)
+    {
+        int nr=Integer.parseInt(s);
+        return nr;
+    }
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
+		int n=rows.length;
+		int minim=0;
+		for(int i=0;i<n;i++)
+		{
+		    int m=rows[i].length;
+		    int t=numberMachines,sum=0;
+		    for(int j=0;j<m;j++)
+		    {
+		        if("X"==rows[i][j])
+		        {
+		            t=numberMachines;
+		            continue;
+		        }
+		        sum+=getnum(rows[i][j]);
+		        --t;
+		        if(t==0)
+		        {
+		            if(minim==0) minim=sum;
+		            else if(minim>sum) minim=sum;
+		            sum-=getnum(rows[i][j-numberMachines+1]);
+		            t=1;
+		         }
+		    }
+		  }
+		return minim;
 	}
 
 }
+
