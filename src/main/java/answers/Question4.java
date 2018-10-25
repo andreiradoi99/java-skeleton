41168448b7415fa3418af626a93571550b01959c
andreiradoi99 <andrei_radoi14@yahoo.com>
2018-10-25 22:54:16 +0100

    q5 updated

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -2,45 +2,39 @@ package answers;
 
 
 
-
-
-
-
-public class Question4
-{
-    public static int selectionFailedTradedesks(String[][] rows, int numberMachines)
+public class Question4 {
+    public static int getnum(String s)
     {
-        int n=rows.length;
-        int minim=-1;
-        for(int i=0; i<n; i++)
-        {
-            int m=rows[i].length;
-            int t=numberMachines,sum=0;
-            for(int j=0; j<m; j++)
-            {
-                if("X"==rows[i][j])
-                {
-                    t=numberMachines;
-                    sum=0;
-                }
-                else
-                {
-                    sum+=Integer.parseInt(rows[i][j]);
-                    --t;
-                    if(t==0)
-                    {
-                        if(minim==-1) minim=sum;
-                        else if(minim>sum) minim=sum;
-                        sum-=Integer.parseInt(rows[i][j-numberMachines+1]);
-                        t=1;
-                    }
-                }
-            }
-        }
-        if(minim==-1) minim=0;
-        return minim;
+        int nr=Integer.parseInt(s);
+        return nr;
     }
+	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
+		int n=rows.length;
+		int minim=0;
+		for(int i=0;i<n;i++)
+		{
+		    int m=rows[i].length;
+		    int t=numberMachines,sum=0;
+		    for(int j=0;j<m;j++)
+		    {
+		        if("X"==rows[i][j])
+		        {
+		            t=numberMachines;
+		            continue;
+		        }
+		        sum+=getnum(rows[i][j]);
+		        --t;
+		        if(t==0)
+		        {
+		            if(minim==0) minim=sum;
+		            else if(minim>sum) minim=sum;
+		            sum-=getnum(rows[i][j-numberMachines+1]);
+		            t=1;
+		         }
+		    }
+		  }
+		return minim;
+	}
 
 }
-
 
--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,21 +1,20 @@
 package answers;
+import java.util.Arrays;
 
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		int []minways;
-		minways = new int[1000004];
+		minways = new int[totalValue];
 		minways[0]=0;
+		Arrays.sort(allowedAllocations);
+		int n=allowedAllocations.length;
 		for(int i=1;i<=totalValue;i++) minways[i]=1000000000;
+		for(int j=n-1;j>=0;--j)
 		for(int i=0;i<=totalValue;i++)
 		{
-		    if(minways[i]==1000000000) continue;
-		    for(int j=0;j<allowedAllocations.length;j++)
-		    {
-		        if(minways[i+allowedAllocations[j]]>minways[i]+1) minways[i+allowedAllocations[j]]=minways[i]+1;
-		     }
+		    if(minways[i]!=1000000000&&minways[i+allowedAllocations[j]]==1000000000) minways[i+allowedAllocations[j]]=minways[i]+1;
 		 }
-		 
 		return minways[totalValue];
 }
 
