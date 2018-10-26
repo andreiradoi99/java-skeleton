1434a1182151026a86f5f8a0e5ed0426ab98e295
andreiradoi99 <andrei_radoi14@yahoo.com>
2018-10-26 15:24:12 +0100

    q5 updated

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,17 +1,17 @@
-package answers;
+pupackage answers;
 import java.util.Arrays;
 
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		int []minways;
-		minways = new int[totalValue];
+		minways = new int[totalValue+5];
 		minways[0]=0;
 		Arrays.sort(allowedAllocations);
 		int n=allowedAllocations.length;
 		for(int i=1;i<=totalValue;i++) minways[i]=1000000000;
 		for(int j=n-1;j>=0;--j)
-		for(int i=0;i<=totalValue;i++)
+		for(int i=0;i<=totalValue-allowedAllocations[j];i++)
 		{
 		    if(minways[i]!=1000000000&&minways[i+allowedAllocations[j]]==1000000000) minways[i+allowedAllocations[j]]=minways[i]+1;
 		 }
